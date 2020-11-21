b8403e93b9e8b2dd4e408a884c8b8bd5d7d9da66
Marcos Henrique <makikin18@hotmail.com>
2020-11-21 18:50:49 -0300

    add

--- scripts/initColapso.js
+++ scripts/initColapso.js
@@ -1,7 +1,6 @@
 // Parte de execucao do interacao entre o titulo e o seus textos
 export default function executarColapso() {
   const colapso = document.querySelectorAll('.js-collapse dt');
-  const colapsoDd = document.querySelectorAll('.js-collapse dd');
   const ativarClasse = 'collapse';
   function execultarColapso() {
     this.classList.toggle(ativarClasse);
@@ -9,9 +8,8 @@ export default function executarColapso() {
   }
 
   if (colapso.length) {
-    // colapso[0].classList.add(ativarClasse);
-    // colapso[0].nextElementSibling.classList.add(ativarClasse);
-    colapsoDd[0].classList.add(ativarClasse);
+    colapso[0].classList.add(ativarClasse);
+    colapso[0].nextElementSibling.classList.add(ativarClasse);
 
     colapso.forEach((item) => {
       item.addEventListener('click', execultarColapso);
